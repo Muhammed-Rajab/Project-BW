0c6d7e66de6083e03bb48bc5122f0638918bf0a9
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-07-27 23:21:26 +0530

    Added way to fetch skills data from github

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { fetchAndUpdateProjectsData } from "./projects.js";
+import { fetchAndUpdateSkillsData } from "./skills.js";
 
 window.addEventListener("load", async () => {
+    await fetchAndUpdateSkillsData();
     await fetchAndUpdateProjectsData();
 });
---
+++ scripts/skills.js
@@ -0,0 +1,31 @@
+import { fetchAndParseJSON } from "./utils.js";
+
+export function generateSkillList(title, skills) {
+    return `<div class="d-flex fd-col skill-list">
+    <span class="skill-title">${title}</span>
+    <div class="d-flex fd-row skills">
+        ${skills
+            .map(
+                (sk) =>
+                    `<div style="border: 4px solid ${sk.color}; color: ${
+                        sk.color
+                    };" class="skill-js">${sk.title.toUpperCase()}</div>`
+            )
+            .join("\n\t\t")}
+    </div>
+</div>`;
+}
+
+export async function fetchAndUpdateSkillsData() {
+    const SKILLS_URL =
+        "https://raw.githubusercontent.com/Muhammed-Rajab/portfolio-data/master/skills.json";
+    const skillListContainer = document.querySelector(".skill-lists-container");
+    const skillsData = await fetchAndParseJSON(SKILLS_URL);
+
+    skillListsHtml = Object.keys(skillsData)
+        .map((key) => generateSkillList(key, skillsData[key]))
+        .join("\n\n");
+    setTimeout(() => {
+        skillListContainer.innerHTML = skillListsHtml;
+    }, 250);
+}
